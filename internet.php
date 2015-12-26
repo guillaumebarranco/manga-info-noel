a70525009772c9b5a195b41c9315f06531ce55c4
guillaumebarranco <guillaume.barranco8@hotmail.fr>
2015-12-26 21:21:39 +0100

    change indices

---
+++ internet.php
@@ -0,0 +1,39 @@
+<!DOCTYPE HTML>
+
+<html>
+	<head>
+
+		<meta charset="utf-8" />
+
+		<title>Une aventure de Noël</title>
+
+		<link rel="shortcut icon" href="img/favicon.ico.png" type="image/x-icon"/> 
+		<link rel="icon" href="img/favicon.ico.png" type="image/x-icon"/>
+		
+		<meta name="viewport" content="width=device-width, initial-scale=1" user-scalable="yes" />
+	
+		<link rel="stylesheet" href="css/index.css" />
+
+	</head>
+
+	<body>
+		
+		<header class="page_epreuve_1">
+			<h1>Indice n°1</h1>
+		</header>
+
+		<div class="wrapper">
+			<p>
+				Ce que vous aimez sur le forum de manga-info ?<br><br>
+				<strong>Le jeu spécial de fin d'année</strong>
+			</p>
+		</div>
+
+
+		<footer>
+			Copyright © Manga-Info 2015
+		</footer>
+
+	</body>
+
+</html>
